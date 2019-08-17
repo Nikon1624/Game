3e9e083772919a24202a83f086c02ae4bd36c6a7
Denis Nikonov <nikon1624@gmail.com>
2019-08-17 17:54:45 +0300

    third

--- script.js
+++ script.js
@@ -1,30 +1,82 @@
 'use strict';
-const gameField = document.querySelector('.game__field');
+
 const reset = document.querySelector('.game__reset');
 const cells = document.querySelectorAll('.game__cell');
-let move = 0;
+const gameTitle = document.querySelector('.game__title');
+const scores = document.querySelector('.game__scores');
+let isCross = true;
 
-gameField.addEventListener('click', evt => {
-  let target = evt.target;
+for (let i = 0; i < cells.length; i++) {
+  cells[i].addEventListener('click', () => {
+    if (!cells[i].classList.contains('game__cell--unclickable') && isCross) {
+      cells[i].classList.add('game__cell--player');
+      cells[i].classList.add('game__cell--unclickable');
 
-  if ( target.classList.contains('game__cell') && move % 2 == 0) {
-    target.classList.add('game__cell--player');
-  }
+      isCross = false;
 
-  if ( target.classList.contains('game__cell') && move % 2 != 0) {
-    target.classList.add('game__cell--pc');
-  }
-  
-  move++;
-});
+      if ( checkWinner() == 'cross' ) {
+        gameTitle.textContent = 'Крестики победили!';
+
+        addResult(1);
+
+        for (let cell of cells) {
+          cell.classList.add('game__cell--unclickable');
+        }
+      }
+    } else if (!cells[i].classList.contains('game__cell--unclickable') && !isCross) {
+      cells[i].classList.add('game__cell--pc');
+      cells[i].classList.add('game__cell--unclickable');
+
+      isCross = true;
+
+      if ( checkWinner() == 'zero' ) {
+        gameTitle.textContent = 'Нолики победили!'; 
+        
+        addResult(3);
+
+        for (let cell of cells) {
+          cell.classList.add('game__cell--unclickable');
+        }
+      }
+    }
+  });
+}
 
 reset.addEventListener('click', () => {
   for (let cell of cells) {
     cell.classList.remove('game__cell--player');
     cell.classList.remove('game__cell--pc');
+    cell.classList.remove('game__cell--unclickable');
+    cell.classList.remove('game__cell--win');
+
+    if (gameTitle.textContent != 'Крестики-нолики') {
+      gameTitle.textContent = 'Крестики-нолики';
+    }
   }
 });
 
 function checkWinner() {
-  
+  const combination = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]];
+
+  for (let i = 0; i < combination.length; i++) {
+    if (cells[combination[i][0]].classList.contains('game__cell--player') && cells[combination[i][1]].classList.contains('game__cell--player') && cells[combination[i][2]].classList.contains('game__cell--player')) {
+      cells[combination[i][0]].classList.add('game__cell--win');
+      cells[combination[i][1]].classList.add('game__cell--win');
+      cells[combination[i][2]].classList.add('game__cell--win');
+
+      return 'cross';
+    } else if (cells[combination[i][0]].classList.contains('game__cell--pc') && cells[combination[i][1]].classList.contains('game__cell--pc') && cells[combination[i][2]].classList.contains('game__cell--pc')) {
+      cells[combination[i][0]].classList.add('game__cell--win');
+      cells[combination[i][1]].classList.add('game__cell--win');
+      cells[combination[i][2]].classList.add('game__cell--win');
+
+      return 'zero';
+    }
+  }
+}
+
+function addResult(index) {
+  let scoresText = scores.textContent.split(' ');
+  scoresText[index] = Number(scoresText[index]) + 1;
+  scores.textContent = scoresText.join(' ');
 }
