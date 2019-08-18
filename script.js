3f543dd0f495fe7849dd3fcaccc3d88a35e9dfe6
Denis Nikonov <nikon1624@gmail.com>
2019-08-18 21:54:51 +0300

    fourth

--- script.js
+++ script.js
@@ -4,75 +4,84 @@ const reset = document.querySelector('.game__reset');
 const cells = document.querySelectorAll('.game__cell');
 const gameTitle = document.querySelector('.game__title');
 const scores = document.querySelector('.game__scores');
+const CROSS_CLASS = 'game__cell--cross';
+const ZERO_CLASS = 'game__cell--zero';
+const WIN_CLASS = 'game__cell--win';
 let isCross = true;
 
-for (let i = 0; i < cells.length; i++) {
-  cells[i].addEventListener('click', () => {
-    if (!cells[i].classList.contains('game__cell--unclickable') && isCross) {
-      cells[i].classList.add('game__cell--player');
-      cells[i].classList.add('game__cell--unclickable');
-
-      isCross = false;
-
-      if ( checkWinner() == 'cross' ) {
-        gameTitle.textContent = 'Крестики победили!';
-
-        addResult(1);
-
-        for (let cell of cells) {
-          cell.classList.add('game__cell--unclickable');
-        }
-      }
-    } else if (!cells[i].classList.contains('game__cell--unclickable') && !isCross) {
-      cells[i].classList.add('game__cell--pc');
-      cells[i].classList.add('game__cell--unclickable');
-
-      isCross = true;
-
-      if ( checkWinner() == 'zero' ) {
-        gameTitle.textContent = 'Нолики победили!'; 
-        
-        addResult(3);
-
-        for (let cell of cells) {
-          cell.classList.add('game__cell--unclickable');
-        }
-      }
-    }
-  });
+for (let cell of cells) {
+  cell.addEventListener('click', onCellClick);
 }
 
 reset.addEventListener('click', () => {
   for (let cell of cells) {
-    cell.classList.remove('game__cell--player');
-    cell.classList.remove('game__cell--pc');
-    cell.classList.remove('game__cell--unclickable');
-    cell.classList.remove('game__cell--win');
-
-    if (gameTitle.textContent != 'Крестики-нолики') {
-      gameTitle.textContent = 'Крестики-нолики';
+    cell.classList.remove(CROSS_CLASS);
+    cell.classList.remove(ZERO_CLASS);
+    cell.classList.remove(WIN_CLASS);
+    
+    for (let cell of cells) {
+      cell.addEventListener('click', onCellClick);
     }
   }
+
+  if (gameTitle.textContent != 'Крестики-нолики') {
+    gameTitle.textContent = 'Крестики-нолики';
+  }
 });
+
+function onCellClick() {
+  if (!this.classList.contains(CROSS_CLASS) && !this.classList.contains(ZERO_CLASS)) {
+    isCross ? this.classList.add(CROSS_CLASS) : this.classList.add(ZERO_CLASS);
+
+    isCross = !isCross;
+
+    switch ( checkWinner() ) {
+      case 'cross':
+        gameTitle.textContent = 'Крестики победили!';
+        addResult(1);
+        removeOnCellClick();
+        break;
+      case 'zero':
+        gameTitle.textContent = 'Нолики победили!'; 
+        addResult(3);
+        removeOnCellClick();
+        break;
+    }
+  }
+}
+
+function removeOnCellClick() {
+  for (let cell of cells) {
+    cell.removeEventListener('click', onCellClick);
+  }
+}
 
 function checkWinner() {
   const combination = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]];
 
   for (let i = 0; i < combination.length; i++) {
-    if (cells[combination[i][0]].classList.contains('game__cell--player') && cells[combination[i][1]].classList.contains('game__cell--player') && cells[combination[i][2]].classList.contains('game__cell--player')) {
-      cells[combination[i][0]].classList.add('game__cell--win');
-      cells[combination[i][1]].classList.add('game__cell--win');
-      cells[combination[i][2]].classList.add('game__cell--win');
+    if (cells[combination[i][0]].classList.contains(CROSS_CLASS) && 
+        cells[combination[i][1]].classList.contains(CROSS_CLASS) && 
+        cells[combination[i][2]].classList.contains(CROSS_CLASS)) {
+      addWinClass(cells[combination[i][0]]);
+      addWinClass(cells[combination[i][1]]);
+      addWinClass(cells[combination[i][2]]); 
 
       return 'cross';
-    } else if (cells[combination[i][0]].classList.contains('game__cell--pc') && cells[combination[i][1]].classList.contains('game__cell--pc') && cells[combination[i][2]].classList.contains('game__cell--pc')) {
-      cells[combination[i][0]].classList.add('game__cell--win');
-      cells[combination[i][1]].classList.add('game__cell--win');
-      cells[combination[i][2]].classList.add('game__cell--win');
+    } else if (cells[combination[i][0]].classList.contains(ZERO_CLASS) && 
+               cells[combination[i][1]].classList.contains(ZERO_CLASS) && 
+               cells[combination[i][2]].classList.contains(ZERO_CLASS)) {
+      addWinClass(cells[combination[i][0]]);
+      addWinClass(cells[combination[i][1]]);
+      addWinClass(cells[combination[i][2]]);                
 
       return 'zero';
     }
   }
+}
+
+function addWinClass(el) {
+  el.classList.add(WIN_CLASS);
 }
 
 function addResult(index) {
