e7e5702fd996236c1ac1a9fe7a03218fd041e63f
Denis Nikonov <nikon1624@gmail.com>
2019-08-20 14:08:12 +0300

    changed the check for winning

--- script.js
+++ script.js
@@ -10,6 +10,7 @@ const CROSS_CLASS = 'game__cell--cross';
 const ZERO_CLASS = 'game__cell--zero';
 const WIN_CLASS = 'game__cell--win';
 const MESSAGE_SHOW_CLASS = 'game__win-message-wrapper--show';
+const gameFieldWidth = Math.sqrt(cells.length);
 const CROSS_INDEX = 0;
 const ZERO_INDEX = 1;
 let isCross = true;
@@ -22,22 +23,10 @@ for (let cell of cells) {
 
       isCross = !isCross;
 
-      switch ( checkWinner() ) {
-        case 'cross':
-          winMessage.textContent = 'Крестики победили!';
-          winMessageWrapper.classList.add(MESSAGE_SHOW_CLASS);
-          addScore(CROSS_INDEX);
-          break;
-        case 'zero':
-          winMessage.textContent = 'Нолики победили!';
-          winMessageWrapper.classList.add(MESSAGE_SHOW_CLASS);
-          addScore(ZERO_INDEX);
-          break;
-      }
+      checkWinner();
 
       if ( checkDraw() ) {
-        winMessage.textContent = 'Ничья';
-        winMessageWrapper.classList.add(MESSAGE_SHOW_CLASS);
+        showWinMessage('Ничья');
       }
     }
   });
@@ -52,6 +41,117 @@ reset.addEventListener('click', () => {
   }
 });
 
+function checkWinner() {
+  for (let i = 0; i < gameFieldWidth; i++) {
+    let colArr = [];
+  
+    for (let j = i; j < cells.length; j += gameFieldWidth) {
+      colArr.push(cells[j]);
+    }
+  
+    if ( checkClass(colArr, CROSS_CLASS) ) {
+      for (let key in colArr) {
+        addWinClass(colArr[key]);
+      }
+      
+      addScore(CROSS_INDEX);
+      showWinMessage('Крестики победили!');
+  
+      break;
+    }
+  
+    if ( checkClass(colArr, ZERO_CLASS) ) {
+      for (let key in colArr) {
+        addWinClass(colArr[key]);
+      }
+  
+      addScore(ZERO_INDEX);
+      showWinMessage('Нолики победили!');
+  
+      break;
+    }
+  }
+
+  for (let i = 0; i <= cells.length - gameFieldWidth; i += gameFieldWidth) {
+
+    let rowArr = [];
+
+    for (let j = i; j < i + gameFieldWidth; j++) {
+      rowArr.push(cells[j])
+    }
+
+    if ( checkClass(rowArr, CROSS_CLASS) ) {
+      for (let key in rowArr) {
+        addWinClass(rowArr[key]);
+      }
+    
+      addScore(CROSS_INDEX);
+      showWinMessage('Крестики победили!');
+    
+      break;
+    }
+    
+    if ( checkClass(rowArr, ZERO_CLASS) ) {
+      for (let key in rowArr) {
+        addWinClass(rowArr[key]);
+      }
+    
+      addScore(ZERO_INDEX);
+      showWinMessage('Нолики победили!');
+    
+      break;
+    }
+  }
+
+  let rightDiagonal = [];
+
+  for (let i = gameFieldWidth - 1; i <= cells.length - gameFieldWidth; i += (gameFieldWidth - 1)) {
+    rightDiagonal.push(cells[i]);
+  }
+
+  if ( checkClass(rightDiagonal, CROSS_CLASS) ) {
+    for (let key in rightDiagonal) {
+      addWinClass(rightDiagonal[key]);
+    }
+
+    addScore(CROSS_INDEX);
+    showWinMessage('Крестики победили!');
+  }
+
+  if ( checkClass(rightDiagonal, ZERO_CLASS) ) {
+    for (let key in rightDiagonal) {
+      addWinClass(rightDiagonal[key]);
+    }
+
+    addScore(ZERO_INDEX);
+    showWinMessage('Нолики победили!');
+  }
+
+  let leftDiagonal = [];
+
+  for (let i = 0; i <= cells.length; i += (gameFieldWidth + 1)) {
+    leftDiagonal.push(cells[i]);
+  }
+
+  if ( checkClass(leftDiagonal, CROSS_CLASS) ) {
+    for (let key in leftDiagonal) {
+      addWinClass(leftDiagonal[key]);
+    }
+
+    addScore(CROSS_INDEX);
+    showWinMessage('Крестики победили!');
+  }
+
+  if ( checkClass(leftDiagonal, ZERO_CLASS) ) {
+    for (let key in leftDiagonal) {
+      addWinClass(leftDiagonal[key]);
+    }
+
+    addScore(ZERO_INDEX);
+    showWinMessage('Нолики победили!');
+  }
+}
+
 function checkDraw() {
   for (let cell of cells) {
     if (cell.classList.length == 1) {
@@ -59,33 +159,18 @@ function checkDraw() {
     }
   }
 
-  if ( !checkWinner() ) {
+  if ( !winMessageWrapper.classList.contains(MESSAGE_SHOW_CLASS) ) {
     return true;
   }
 }
 
-function checkWinner() {
-  const combination = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 4, 8], [2, 4, 6], [0, 3, 6], [1, 4, 7], [2, 5, 8]];
+function showWinMessage(message) {
+  winMessage.textContent = message;
+  winMessageWrapper.classList.add(MESSAGE_SHOW_CLASS);
+}
 
-  for (let i = 0; i < combination.length; i++) {
-    if ( cells[combination[i][0]].classList.contains(CROSS_CLASS) &&
-      cells[combination[i][1]].classList.contains(CROSS_CLASS) &&
-      cells[combination[i][2]].classList.contains(CROSS_CLASS) ) {
-      addWinClass(cells[combination[i][0]]);
-      addWinClass(cells[combination[i][1]]);
-      addWinClass(cells[combination[i][2]]);
-
-      return 'cross';
-    } else if ( cells[combination[i][0]].classList.contains(ZERO_CLASS) &&
-      cells[combination[i][1]].classList.contains(ZERO_CLASS) &&
-      cells[combination[i][2]].classList.contains(ZERO_CLASS) ) {
-      addWinClass(cells[combination[i][0]]);
-      addWinClass(cells[combination[i][1]]);
-      addWinClass(cells[combination[i][2]]);
-
-      return 'zero';
-    }
-  }
+function checkClass(arr, className) {
+  return arr.every(item => item.classList.contains(className) ? true : false);
 }
 
 function addWinClass(el) {
